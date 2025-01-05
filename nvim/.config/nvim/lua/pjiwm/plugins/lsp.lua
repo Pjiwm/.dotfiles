014bf9acf7215c4a38dd8bec5eac2795fae56e50
Pjiwm <63792136+Pjiwm@users.noreply.github.com>
2025-01-05 18:16:22 +0100

    display diagnostics with gl for nvim

--- nvim/.config/nvim/lua/pjiwm/plugins/lsp.lua
+++ nvim/.config/nvim/lua/pjiwm/plugins/lsp.lua
@@ -144,6 +144,7 @@ return {
             local hl = 'DiagnosticSign' .. type
             vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
         end
+        vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { noremap = true, silent = true })
         vim.diagnostic.config({
             -- update_in_insert = true,
             float = {
