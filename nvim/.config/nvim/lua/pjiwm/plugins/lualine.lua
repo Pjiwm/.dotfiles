1f89e72ccd50679142e4357e348acbde51334921
Pjiwm <63792136+Pjiwm@users.noreply.github.com>
2025-01-12 18:56:13 +0100

    modified lualine

--- nvim/.config/nvim/lua/pjiwm/plugins/lualine.lua
+++ nvim/.config/nvim/lua/pjiwm/plugins/lualine.lua
@@ -2,13 +2,13 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        vim.opt.cmdheight = 0
         require("lualine").setup {
             options = {
                 icons_enabled = true,
                 theme = "dracula",
-                component_separators = { left = "", right = "" },
-                section_separators = { left = "", right = "" },
+                -- component_separators = { left = "", right = "" },
+                component_separators = { left = "|", right = "|" },
+                section_separators = { left = "", right = "" },
                 disabled_filetypes = {
                     statusline = { "NvimTree" },
                     winbar = { "NvimTree" },
@@ -25,19 +25,11 @@ return {
             },
             sections = {
                 lualine_a = { "mode" },
-                lualine_b = { "branch", "diff", "diagnostics" },
+                lualine_b = { "diagnostics", "diff" },
                 lualine_c = { "filename" },
-                lualine_x = { "encoding", "fileformat", "filetype" },
+                lualine_x = { "filetype", "encoding" },
                 lualine_y = { "progress" },
                 lualine_z = { "location" },
-            },
-            inactive_sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = { "filename" },
-                lualine_x = { "location" },
-                lualine_y = {},
-                lualine_z = {},
             },
             tabline = {},
             winbar = {},
